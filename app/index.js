aa56fe0c403828e1e9443132ce617385e637a853
pikatos <pikatos@gmail.com>
2020-07-13 17:44:34 +0200

    add basic reason react native environment

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+
+const APP_NAME = "ShowcaseWallet";
+
+AppRegistry.registerComponent(APP_NAME, () => App);
---
+++ app/windows/ShowcaseWallet/App.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+
+
+namespace winrt::ShowcaseWallet::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::ShowcaseWallet::implementation
+
+
