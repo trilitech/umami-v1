658b9e998d0cd502470dc92294a5f8d5c0968648
freddy <freddy03h@gmail.com>
2021-01-21 11:03:36 +0100

    apply willChange transform to flippedView to avoid repainting during animation

--- src/bindings/ViewWithFlippedProps.js
+++ src/bindings/ViewWithFlippedProps.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({"view": {willChange: "transform"}});
 
 const ViewWithFlippedProps = ({children, flippedProps}) => {
   const dataSet = Object.fromEntries(
     Object.entries(flippedProps).map(([key, value]) => [key.replace("data-",""),  value])
   );
-  return (<View dataSet={dataSet} >{children}</View>);
+  return (<View style={styles.view} dataSet={dataSet} >{children}</View>);
 };
 
 export default ViewWithFlippedProps;
