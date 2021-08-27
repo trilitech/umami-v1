8f5230748e292a630b4391f383fc7f092907597a
Rémy El Sibaïe <remy@nomadic-labs.com>
2021-08-27 11:40:04 +0200

    Build system: vendoring taquito-signer as is

---
+++ src/vendors/signer/dist/lib/version.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VERSION = void 0;
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+exports.VERSION = {
+    "commitHash": "33283ebb64d801dcbb0926ad199d024ad7189861",
+    "version": "10.1.0"
+};
+/* tslint:enable */
+//# sourceMappingURL=version.js.map
