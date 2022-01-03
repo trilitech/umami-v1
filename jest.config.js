17886228c9067e3552c3187cf665f1c1a303a85a
Lev Kowalski <lkowalski@accengage.com>
2022-01-03 17:41:14 +0100

    format jsConfig

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  "moduleDirectories": [
-    "node_modules"
+  moduleDirectories: ["node_modules"],
+  testMatch: ["**/__tests__/*_test.bs.js"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(bs-platform|reason-react|reason-react-compat|reason-react-update|bs-webapi|bs-css|bs-css-emotion)/)",
   ],
-  "testMatch": [
-    "**/__tests__/*_test.bs.js"
-  ],
-  "transformIgnorePatterns": [
-    "node_modules/(?!(bs-platform|reason-react|reason-react-compat|reason-react-update|bs-webapi|bs-css|bs-css-emotion)/)"
-  ],
-  "collectCoverageFrom": [
-	  "src/*.js"
-  ],
-  testEnvironment: 'node'
+  collectCoverageFrom: ["src/*.js"],
+  testEnvironment: "node",
 };
