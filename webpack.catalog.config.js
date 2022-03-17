f79f913384af24e0f689188fc05c44373f9e13c8
Lev Kowalski <levkowalski@gmail.com>
2022-03-17 16:28:00 +0100

    minimal webpack config for catalog

---
+++ webpack.catalog.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/catalog/catalogIndex.bs.js",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  devtool: "cheap-source-map",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: [
+      path.resolve(__dirname, "public"),
+      path.resolve(__dirname, "node_modules"),
+    ],
+  },
+  resolve: {
+    // This will only alias the exact import "react-native"
+    alias: {
+      "react-native$": "react-native-web",
+    },
+    // If you're working on a multi-platform React Native app, web-specific
+    // module implementations should be written in files using the extension
+    // `.web.js`.
+    extensions: [".web.js", ".js", ".bs.js"],
+  },
+};
