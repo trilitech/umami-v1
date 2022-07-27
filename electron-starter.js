96db6c3bf84c94da1c2fab75237a4f05d5dcbee7
Julien SAGOT <contact@sagotch.fr>
2022-07-27 18:27:45 +0200

    app.allowRendererProcessReuse has been removed in Electron 14.0

--- electron-starter.js
+++ electron-starter.js
@@ -155,7 +155,6 @@ function createWindow() {
 }
 
 app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors');
-app.allowRendererProcessReuse = false;
 
 if (!app.isDefaultProtocolClient('umami')) {
   // Define custom protocol handler. Deep linking works on packaged versions of the application!
